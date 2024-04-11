0f7f3b857514031a18809bf3674a7482931ae02c
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-04-11 07:50:39 +0530

    Created signup and login using jwt token[ADD]

---
+++ frontend/src/Auth/Login.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Card,Flex,Typography,Form,Input,Button,Alert,Spin } from 'antd'
+import { Link } from 'react-router-dom';
+import useLogin from '../hooks/useLogin';
+
+const Login = () => {
+  const { error,loading, loginUser } = useLogin();
+  const handleLogin = async (values) => {
+    await loginUser(values);
+  }
+  return (
+    <Card className='form-container'>
+       <Flex>
+        {/* form */}
+        <Flex vertical flex={1}>
+            <Typography.Title level={3} strong className='title'>
+                Sign In
+            </Typography.Title>
+            <Typography.Text type="secondary" strong className="slogan">
+                Unlock your World!
+            </Typography.Text>
+            <Form layout='vertical' onFinish={handleLogin} autoComplete="off">
+              <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                required: true,
+                message: 'Please input your Email',
+              },
+              {
+                type: 'email',
+                message: 'The input is not valid Email',
+              },
+              ]}
+              >
+                <Input size="large" placeholder="Enter your Email" />
+              </Form.Item>
+              <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                required: true,
+                message: 'please input your password',
+              },
+              ]}
+              >
+                <Input.Password size="large" placeholder="Enter your password" />
+              </Form.Item>
+
+              {error && (
+                <Alert 
+                description={error} 
+                type="error" 
+                showIcon 
+                closable 
+                className="alert"
+                /> 
+              )}
+              <Form.Item>
+                <Button
+                type={`${loading ? '' : 'primary'}`}
+                htmlType="submit"
+                size="large"
+                className="btn"
+                >
+                  {loading ? <Spin /> : 'Sign in'}
+                </Button>
+              </Form.Item>
+              <Form.Item>
+              <Link to="/">
+                <Button size="large" className="btn">
+                  Create an Account
+                </Button>
+              </Link>
+              </Form.Item>
+            </Form>
+        </Flex>
+
+        {/* image */}
+        <Flex>
+
+        </Flex>
+       </Flex>
+    </Card>
+  )
+}
+
+export default Login
---
+++ frontend/src/Auth/Register.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import { Card,Flex,Typography,Form,Input,Button,Alert,Spin } from 'antd'
+import { Link } from 'react-router-dom';
+import useSignup from '../hooks/useSignup';
+
+const Register = () => {
+  const { loading, error, registerUser } = useSignup();
+  const handleRegister = (values) => {
+     registerUser(values);
+  };
+
+  return (
+    <Card className='form-container'>
+       <Flex gap="large" align="center">
+        {/* form */}
+        <Flex vertical flex={1}>
+            <Typography.Title level={3} strong className='title'>
+                Create an Account
+            </Typography.Title>
+            <Typography.Text type="secondary" strong className="slogan">
+                Join into our community!
+            </Typography.Text>
+            <Form layout='vertical' onFinish={handleRegister} autoComplete="off">
+              <Form.Item
+              label="Full name"
+              name="name"
+              rules={[
+                {
+                required: true,
+                message: 'Please input your full name',
+              },
+              ]}
+              >
+                <Input size="large" placeholder="Enter your full name" />
+              </Form.Item>
+              <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                required: true,
+                message: 'Please input your Email',
+              },
+              {
+                type: 'email',
+                message: 'The input is not valid Email',
+              },
+              ]}
+              >
+                <Input size="large" placeholder="Enter your Email" />
+              </Form.Item>
+              <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                required: true,
+                message: 'please input your password',
+              },
+              ]}
+              >
+                <Input.Password size="large" placeholder="Enter your password" />
+              </Form.Item>
+              <Form.Item
+              label="Password"
+              name="passwordConfirm"
+              rules={[
+                {
+                required: true,
+                message: 'please input your confirm password',
+              },
+              ]}
+              >
+                <Input.Password size="large" placeholder="Re-enter your password" />
+              </Form.Item>
+
+              {error && (
+                <Alert 
+                description={error} 
+                type="error" 
+                showIcon 
+                closable 
+                className="alert"
+                /> 
+              )}
+              <Form.Item>
+                <Button
+                type={`${loading ? '' : 'primary'}`}
+                htmlType="submit"
+                size="large"
+                className="btn"
+                >
+                  {loading ? <Spin /> : 'Create Account'}
+                </Button>
+              </Form.Item>
+              <Form.Item>
+              <Link to="/login">
+                <Button size="large" className="btn">
+                  Sign In
+                </Button>
+              </Link>
+              </Form.Item>
+            </Form>
+        </Flex>
+
+        {/* image */}
+        <Flex>
+
+        </Flex>
+       </Flex>
+    </Card>
+  )
+}
+
+export default Register
