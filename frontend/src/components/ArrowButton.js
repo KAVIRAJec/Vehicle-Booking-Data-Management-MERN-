829711a510991a47c7e9548384e9e72dd2ecce49
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-07-31 12:01:49 +0530

    added ArrowButton[FIX]

---
+++ frontend/src/components/ArrowButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// Define the styled component
+const RV = styled.div.withConfig({ displayName: "ArrowButton__ButtonWrapper", componentId: "sc-1ikb0hj-0" })(["@media screen and (max-width:", "px){display:none;}"]);
+
+// Define the ArrowButton component
+const ArrowButton = () => {
+  return (
+    <RV>
+      
+    </RV>
+  );
+};
+
+export default ArrowButton;
